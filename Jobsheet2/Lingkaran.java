039375654ec4ff4d53acae4cf20cf5ff5aa8e6a9
kanaya06.dwi <kanayadwiyuniarahmawati.com>
2026-09-04 11:00:33 +0700

    Tugas Nomor 3

---
+++ Jobsheet2/Lingkaran.java
@@ -0,0 +1,15 @@
+package Jobsheet2;
+
+public class Lingkaran {
+    public double phi;
+    public double r;
+
+    public void hitungLuas(){
+        double luas = phi * r * r;
+        System.out.println("Luas Lingkaran : " + luas);
+    }
+    public void hitungKeliling(){
+        double keliling = 2 * phi * r;
+        System.out.println("Keliling Lingkaran : " + keliling);
+    }
+}
---
+++ Jobsheet2/TestLingkaran.java
@@ -0,0 +1,11 @@
+package Jobsheet2;
+
+public class TestLingkaran {
+    public static void main(String[] args) {
+        Lingkaran l1 = new Lingkaran();
+        l1.phi = 3.14;
+        l1.r = 10;
+        l1.hitungLuas();
+        l1.hitungKeliling();
+    }
+}
