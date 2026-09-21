210e1c04cf624aabaaabd721438fee3c35c71eb4
kanaya06.dwi <kanayadwiyuniarahmawati.com>
2026-09-21 20:48:16 +0700

    Percobaan 5

---
+++ Jobsheet4/id/ac/polinema/relasiclass/percobaan5/MainPercobaan5.java
@@ -0,0 +1,8 @@
+package id.ac.polinema.relasiclass.percobaan5;
+
+public class MainPercobaan5 {
+    public static void main(String[] args) {
+        Mobil mobil = new Mobil("Avanxa");
+        mobil.tampilkanInfo();
+    }
+}
---
+++ Jobsheet4/id/ac/polinema/relasiclass/percobaan5/Mesin.java
@@ -0,0 +1,12 @@
+package id.ac.polinema.relasiclass.percobaan5;
+
+public class Mesin {
+    private String tipe;
+
+    public Mesin() {
+        this.tipe = "4-silinder";
+    }
+    public String getTipe() {
+        return tipe;
+    }
+}
